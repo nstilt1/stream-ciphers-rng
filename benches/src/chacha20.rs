c9fc2993386d0fcd87241cb88f444cd6bdc9a2b6
Noah Stiltner <nstilt1@lsu.edu>
2024-01-15 14:37:41 -0600

    cleaned up benches a little bit

--- benches/src/chacha20.rs
+++ benches/src/chacha20.rs
@@ -1,38 +1,14 @@
 //! ChaCha20 benchmark
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use benches::{criterion_group_bench, Benchmarker};
 
-#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
-use criterion_cycles_per_byte::CyclesPerByte;
-
-//use previous_chacha20::{
 use chacha20::{
     cipher::{KeyIvInit, StreamCipher},
     ChaCha20,
 };
 
 const KB: usize = 1024;
-#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
-fn bench(c: &mut Criterion<CyclesPerByte>) {
-    let mut group = c.benchmark_group("stream-cipher");
-
-    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
-        let mut buf = vec![0u8; *size];
-
-        group.throughput(Throughput::Bytes(*size as u64));
-
-        group.bench_function(BenchmarkId::new("apply_keystream", size), |b| {
-            let key = Default::default();
-            let nonce = Default::default();
-            let mut cipher = ChaCha20::new(&key, &nonce);
-            b.iter(|| cipher.apply_keystream(&mut buf));
-        });
-    }
-
-    group.finish();
-}
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
-fn bench(c: &mut Criterion) {
+fn bench(c: &mut Benchmarker) {
     let mut group = c.benchmark_group("stream-cipher");
 
     for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
@@ -53,69 +29,30 @@ fn bench(c: &mut Criterion) {
 
 use chacha20::rand_core::{RngCore, SeedableRng};
 
-#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
-fn bench_chacha20rng(c: &mut Criterion<CyclesPerByte>) {
-    let mut chacha_x86 = c.benchmark_group("ChaCha20Rng");
-
-    // no SIMD first
+fn bench_chacha20rng(c: &mut Benchmarker) {
+    let mut chacha = c.benchmark_group("ChaCha20Rng");
+    
     for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
         let mut buf = vec![0u8; *size];
 
-        chacha_x86.throughput(Throughput::Bytes(*size as u64));
+        chacha.throughput(Throughput::Bytes(*size as u64));
 
-        chacha_x86.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
+        chacha.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
             let mut rng = chacha20::ChaCha20Rng::from_seed([0u8; 32]);
             b.iter(|| rng.fill_bytes(&mut buf));
         });
     }
 
-    chacha_x86.finish();
+    chacha.finish();
 }
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
-fn bench_chacha20rng(c: &mut Criterion) {
-    let mut chacha_aarch64 = c.benchmark_group("ChaCha20Rng");
-
-    // no SIMD first
-    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
-        let mut buf = vec![0u8; *size];
-
-        chacha_aarch64.throughput(Throughput::Bytes(*size as u64));
-
-        chacha_aarch64.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
-            let mut rng = chacha20::ChaCha20Rng::from_seed([0u8; 32].into());
-            b.iter(|| rng.fill_bytes(&mut buf));
-        });
-    }
-
-    chacha_aarch64.finish();
-}
-
-#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
-criterion_group!(
-    name = benches_chacha20rng;
-    config = Criterion::default().with_measurement(CyclesPerByte);
-    targets = bench_chacha20rng
+criterion_group_bench!(
+    benches_chacha20rng,
+    bench_chacha20rng
 );
 
-#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
-criterion_group!(
-    name = benches;
-    config = Criterion::default().with_measurement(CyclesPerByte);
-    targets = bench
+criterion_group_bench!(
+    benches,
+    bench
 );
 
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
-criterion_group!(
-    name = benches_chacha20rng;
-    config = Criterion::default();
-    targets = bench_chacha20rng
-);
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
-criterion_group!(
-    name = benches;
-    config = Criterion::default();
-    targets = bench
-);
 criterion_main!(benches, benches_chacha20rng);
